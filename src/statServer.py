635e6f8d89be1d889a8bce14e7f1220d7e329485
suobset <kushagra2410@gmail.com>
2023-12-02 14:11:45 -0500

    Basic server works

--- src/statServer.py
+++ src/statServer.py
@@ -5,38 +5,39 @@ import tkinter as tk
 import statInterface
 import http.server
 import socketserver
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+PORT = 8000
 
-class RequestHandler(BaseHTTPRequestHandler):
+class Server(http.server.BaseHTTPRequestHandler):
     def _set_response(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
-
+    
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length).decode('utf-8')
-
-        print("Received POST request data:")
-        print(post_data)
+        post_data = self.rfile.read(content_length)
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+            str(self.path), str(self.headers), post_data.decode('utf-8'))
 
         self._set_response()
-        self.wfile.write("Data received successfully!\n".encode('utf-8'))
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
 
-def run_server(port=8000):
+def run(server_class=http.server.HTTPServer, handler_class=Server, port=8000):
+    logging.basicConfig(level=logging.INFO)
     server_address = ('', port)
-    httpd = HTTPServer(server_address, RequestHandler)
-    print(f'Starting server on port {port}...')
+    httpd = server_class(server_address, handler_class)
+    logging.info('Starting httpd...\n')
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
     httpd.server_close()
-    print('Server stopped.')
+    logging.info('Stopping httpd...\n')
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = statInterface.SaberStatUI(root)
-    #root.mainloop()
     ###
-    run_server()
+    run()
+    root.mainloop()
